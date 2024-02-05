147d765c1bc2da71c3cb5444f0cbcb52f479dd8e
anushakarthick <anushakarthick@localhost>
2024-02-05 14:04:37 -0500

    Changed initelements in TestBase

--- src/test/java/dsutilities/TestBase.java
+++ src/test/java/dsutilities/TestBase.java
@@ -7,9 +7,14 @@ import java.util.Properties;
 import org.openqa.selenium.support.PageFactory;
 
 import dsalgoPOM.ArrayPage;
+import dsalgoPOM.CommonPOM;
 import dsalgoPOM.Datastructures;
+import dsalgoPOM.GraphPage;
 import dsalgoPOM.HomePage;
+import dsalgoPOM.LinkListPage;
+import dsalgoPOM.QueuePage;
 import dsalgoPOM.RegisterationLoginPage;
+import dsalgoPOM.StackPage;
 import dsalgoPOM.TreePage;
 import dsalgo_webdriver_manager.DriverManager;
 
@@ -42,11 +47,16 @@ public class TestBase {
 	}
 	
 	public static void initElements() {
-		//PageFactory.initElements(DriverManager.getDriver(),HomePage.class);
-		//PageFactory.initElements(DriverManager.getDriver(),RegisterationLoginPage.class);
+		PageFactory.initElements(DriverManager.getDriver(),HomePage.class);
+		PageFactory.initElements(DriverManager.getDriver(),RegisterationLoginPage.class);
 		PageFactory.initElements(DriverManager.getDriver(),Datastructures.class);
-		//PageFactory.initElements(DriverManager.getDriver(),TreePage.class);
+		PageFactory.initElements(DriverManager.getDriver(),TreePage.class);
 		PageFactory.initElements(DriverManager.getDriver(),ArrayPage.class);
+		PageFactory.initElements(DriverManager.getDriver(),CommonPOM.class);
+		PageFactory.initElements(DriverManager.getDriver(),GraphPage.class);
+		PageFactory.initElements(DriverManager.getDriver(),StackPage.class);
+		PageFactory.initElements(DriverManager.getDriver(),QueuePage.class);
+		PageFactory.initElements(DriverManager.getDriver(),LinkListPage.class);
 	}
 
 	
